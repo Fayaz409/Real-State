c0fca04179c6230510fe6b737a0cfcc0d095c092
Fayaz-21 <fayaztunio06@gmail.com>
2023-10-02 19:30:41 +0500

    Starting new Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+ // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='display-flex my-32 mx-30 text-5xl text-red-400'>Hello World</div>
+    </>
+  )
+}
+
+export default App
